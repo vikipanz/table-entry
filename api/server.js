9530ae60ba54a90f9f2e3d94a008a90478e22130
vikrant-pandey <vikrant.pandey@necsws.com>
2024-11-20 12:23:18 +0530

    changed db

--- api/server.js
+++ api/server.js
@@ -4,6 +4,10 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+  '/weather/:resource/:id/show': '/:weather/:id'
+}))
 server.use(router);
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
